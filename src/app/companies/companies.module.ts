66706ecf4d88ebf15963cfdf82f637fc25d8cc24
dome4 <dominic.seitz@gmx.de>
2020-05-24 23:43:23 +0200

    create companies list component

--- src/app/companies/companies.module.ts
+++ src/app/companies/companies.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CompaniesComponent } from './companies.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 
 const COMPONENTS = [
-  CompaniesComponent
+  CompaniesComponent,
+  CompaniesListComponent
 ];
 
-const ROUTES: Routes = [{ path: '', component: CompaniesComponent }];
+const ROUTES: Routes = [
+  {
+    path: '', component: CompaniesComponent, children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: CompaniesListComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [...COMPONENTS],
